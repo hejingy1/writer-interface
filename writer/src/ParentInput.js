b21480ee759d553d6536e80d82e333b67223a465
hejingyuan10@gmail.com <hejingyuan10@gmail.com>
2023-11-05 15:43:21 -0500

    checking if this is updated

--- writer/src/ParentInput.js
+++ writer/src/ParentInput.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import InputBox from './InputBox';
 import TextBox from './TextBox';
 import './ParentInput.css';
@@ -17,7 +17,7 @@ function ParentInput() {
 
 
 
-  const handleOutlineChange = (index, value) =>{
+  const handleOutlineChange = (index, value) => {
     const updateOutline = [...outlineData];
     updateOutline[index] = value;
     setOutline(updateOutline);
@@ -29,19 +29,19 @@ function ParentInput() {
     setOutlineSeg(updatedData);
   };
 
-  const handleTextChange = (index, value) =>{
+  const handleTextChange = (index, value) => {
     const updateOutline = [...text];
     updateOutline[index] = value;
     setText(updateOutline);
   }
 
-  const handleMaterialChange = (value) =>{
+  const handleMaterialChange = (value) => {
     setMaterial(value);
   }
-  const handleRequirementChange = (value) =>{
+  const handleRequirementChange = (value) => {
     setRequirement(value);
   }
-  const handleWordsChange = (value) =>{
+  const handleWordsChange = (value) => {
     setWords(value);
   }
 
@@ -51,35 +51,35 @@ function ParentInput() {
     "requirement": requirement
   }
 
-  const textFeed ={
+  const textFeed = {
     "requirement_word": requirementWords,
     "material": material,
     "requirement": requirement,
     "outline": outlineData[outlineIndex]
   }
 
-  const handleReturnOutline = (res) =>{
+  const handleReturnOutline = (res) => {
     setOutline(res.data);
   }
 
-  const handleReturnText = (res) =>{
+  const handleReturnText = (res) => {
     console.log(res)
     setOutlineSeg(res.data[0]);
     setText(res.data[1]);
   }
 
-  const handleOnClick = (newIndex) =>{
+  const handleOnClick = (newIndex) => {
     setOutlineIndex(newIndex);
   }
 
   const handleAdd = () => {
     axios.post("http://localhost:8000/api/writers/", outlineFeed)
-    .then((res)=>handleReturnOutline(res));
+      .then((res) => handleReturnOutline(res));
   };
 
-  const handleText = () =>{
+  const handleText = () => {
     axios.put("http://localhost:8000/api/writers/1/", textFeed)
-    .then((res)=>handleReturnText(res));
+      .then((res) => handleReturnText(res));
   }
 
 
@@ -96,18 +96,18 @@ function ParentInput() {
       const outline1 = document.getElementById("outline1");
       const outline2 = document.getElementById("outline2");
       const outline3 = document.getElementById("outline3");
-      outline1.rows = Math.floor(window.innerHeight/45);
-      outline1.cols = Math.floor(window.innerWidth/23);
-      outline2.rows = Math.floor(window.innerHeight/45);
-      outline2.cols = Math.floor(window.innerWidth/23);
-      outline3.rows = Math.floor(window.innerHeight/45);
-      outline3.cols = Math.floor(window.innerWidth/23.5);
+      outline1.rows = Math.floor(window.innerHeight / 45);
+      outline1.cols = Math.floor(window.innerWidth / 23);
+      outline2.rows = Math.floor(window.innerHeight / 45);
+      outline2.cols = Math.floor(window.innerWidth / 23);
+      outline3.rows = Math.floor(window.innerHeight / 45);
+      outline3.cols = Math.floor(window.innerWidth / 23.5);
       a.rows = Math.floor(window.innerHeight / 30);
-      a.cols = Math.floor(window.innerWidth / 12.5); 
+      a.cols = Math.floor(window.innerWidth / 12.5);
       u.rows = Math.floor(window.innerHeight / 60);
-      u.cols = Math.floor(window.innerWidth / 20); 
+      u.cols = Math.floor(window.innerWidth / 20);
       s.rows = Math.floor(window.innerHeight / 70);
-      s.cols = Math.floor(window.innerWidth / 20); 
+      s.cols = Math.floor(window.innerWidth / 20);
     };
 
     // Set initial size
@@ -125,36 +125,36 @@ function ParentInput() {
   return (
     <div>
       <div className="flexbox-container">
-      <label>
-        资料:
-        <InputBox
-          className="border border-black p-2"
-          id={'assistant'}
-          value={material}
-          onChange={(newValue) => handleMaterialChange(newValue)}
+        <label>
+          资料:
+          <InputBox
+            className="border border-black p-2"
+            id={'assistant'}
+            value={material}
+            onChange={(newValue) => handleMaterialChange(newValue)}
           // left = "150"
           // size_row = "20"
           // size_col = "120"
-        />
-      </label>
-      <div className="flexbox-inside">
-        <label>
-          论文要求:
-        <InputBox
-            id={'user'}
-            value={requirement}
-            onChange={(newValue) => handleRequirementChange(newValue)}
           />
         </label>
-        <label>
-          论文字数:
-          <InputBox
-            id={'system'}
-            value={requirementWords}
-            onChange={(newValue) => handleWordsChange(newValue)}
-          />
-        </label>
-      </div>
+        <div className="flexbox-inside">
+          <label>
+            论文要求:
+            <InputBox
+              id={'user'}
+              value={requirement}
+              onChange={(newValue) => handleRequirementChange(newValue)}
+            />
+          </label>
+          <label>
+            论文字数:
+            <InputBox
+              id={'system'}
+              value={requirementWords}
+              onChange={(newValue) => handleWordsChange(newValue)}
+            />
+          </label>
+        </div>
       </div>
       <button onClick={handleAdd}>生成大纲</button>
       <div className="flexbox-container">
@@ -184,11 +184,11 @@ function ParentInput() {
       <ul>
         {outlineSeg?.map((item, index) => (
           <TextBox
-          key={index}
-          onChangeOutline={(newValue) => handleReturnOutlineChange(index, newValue)}
-          onChangePaper={(newValue) => handleTextChange(index, newValue)}
-          outline={item}
-          paper={text[index]}
+            key={index}
+            onChangeOutline={(newValue) => handleReturnOutlineChange(index, newValue)}
+            onChangePaper={(newValue) => handleTextChange(index, newValue)}
+            outline={item}
+            paper={text[index]}
           />
         ))}
       </ul>
